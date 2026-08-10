6e3802cb88ef950433e33a9f059fe0a75ae55634
extremeradev <extremeradev@gmail.com>
2026-08-11 00:38:47 +0200

    deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import Header from "./components/header/Header";
 import CookiesBanner from "./components/cookies/CookiesBanner";
-import { HashRouter, Routes, Route } from "react-router-dom"; // <-- Cambiado de BrowserRouter a HashRouter
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ServicesPage from "./pages/ServicesPage";
 import SobreMiPage from "./pages/SobreMiPage";
 import ContactoPage from "./pages/ContactoPage";
 import Footer from "./components/footer/Footer";
 
+
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Header />
 
       <CookiesBanner />
@@ -21,7 +22,7 @@ function App() {
       </Routes>
 
       <Footer />
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vite.js.org/config/
 export default defineConfig({
   plugins: [react()],
+  appType: 'spa', // <-- AÑADE ESTA LÍNEA
   server: {
-    proxy: {
-      "/api": "http://localhost:8080",
-    },
-  },
+    host: '0.0.0.0', // Mantén las opciones de server que ya tenías
+  }
 })
